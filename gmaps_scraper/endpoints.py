e275f7fc3df24803611e6ebd9dba167f108df3cc
Jinef <80605992+jinef-john@users.noreply.github.com>
2026-04-20 21:47:56 +0300

    refactor: simplify place_url function

--- gmaps_scraper/endpoints.py
+++ gmaps_scraper/endpoints.py
@@ -79,48 +79,16 @@ def search_url(query, lat, lng, zoom=13, lang="en", gl="us", page_size=20, start
 
 
 def place_url(place_id, lat, lng, query="", lang="en", gl="us"):
-    """Build the /maps/preview/place URL for full place details.
-
-    Args:
-        place_id: The hex place ID (e.g. "0x182f17eb1d447363:0x17a2d29bdcf01fda")
-        lat: Place latitude
-        lng: Place longitude
-        query: Original search query for context
-        lang: Language code
-        gl: Country code
-    """
+    """Build the /maps/preview/place URL for full place details."""
     encoded_place_id = quote(place_id, safe="")
     encoded_query = quote(query, safe="") if query else ""
 
     pb = (
-        f"!1m24!1s{encoded_place_id}"
-        f"!3m12!1m3!1d96061!2d{lng}!3d{lat}"
-        f"!2m3!1f0!2f0!3f0!3m2!1i440!2i593!4f13.1"
+        f"!1m6"
+        f"!1s{encoded_place_id}"
+        f"!3m1!1d1000"
         f"!4m2!3d{lat}!4d{lng}"
-        "!12m4!2m3!1i360!2i120!4i8"
-        "!13m57!2m2!1i203!2i100!3m2!2i4!5b1"
-        "!6m6!1m2!1i86!2i86!1m2!1i408!2i240"
-        "!7m33!1m3!1e1!2b0!3e3!1m3!1e2!2b1!3e2!1m3!1e2!2b0!3e3"
-        "!1m3!1e8!2b0!3e3!1m3!1e10!2b0!3e3!1m3!1e10!2b1!3e2"
-        "!1m3!1e10!2b0!3e4!1m3!1e9!2b1!3e2!2b1!9b0"
-        "!15m8!1m7!1m2!1m1!1e2!2m2!1i195!2i195!3i20"
-        "!14m2!1sdummy!7e81"
-        "!15m111!1m29!4e2!13m9!2b1!3b1!4b1!6i1!8b1!9b1!14b1!20b1!25b1"
-        "!18m17!3b1!4b1!5b1!6b1!9b1!13b1!14b1!17b1!20b1!21b1!22b1"
-        "!30b1!32b1!33m1!1b1!34b1!36e2"
-        "!10m1!8e3!11m1!3e1!17b1!20m2!1e3!1e6"
-        "!24b1!25b1!26b1!27b1!29b1!30m1!2b1!36b1!37b1"
-        "!39m3!2m2!2i1!3i1!43b1!52b1!54m1!1b1!55b1!56m1!1b1"
-        "!61m2!1m1!1e1!65m5!3m4!1m3!1m2!1i224!2i298"
-        "!72m22!1m8!2b1!5b1!7b1!12m4!1b1!2b1!4m1!1e1!4b1"
-        "!8m10!1m6!4m1!1e1!4m1!1e3!4m1!1e4"
-        "!3sother_user_google_review_posts__and__hotel_and_vr_partner_review_posts"
-        "!6m1!1e1!9b1!89b1!90m2!1m1!1e2"
-        "!98m3!1b1!2b1!3b1!103b1!113b1!114m3!1b1!2m1!1b1"
-        "!117b1!122m1!1b1!126b1!127b1!128m1!1b0"
-        "!21m0!22m2!1e81!8e4!29m0"
-        "!30m6!3b1!6m1!2b1!7m1!2b1!9b1"
-        "!34m5!7b1!10b1!14b1!15m1!1b0!37i775"
+        "!3m1!1e3"
     )
 
     url = f"{BASE}/maps/preview/place?authuser=0&hl={lang}&gl={gl}&pb={pb}"
